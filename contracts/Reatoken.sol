33178f168813d4a99d18daa257a767bb3088224f
thanh quach <thanhb.quach@gmail.com>
2022-05-17 17:15:14 -0400

    update fee calculations and internal USD representation

--- contracts/Reatoken.sol
+++ contracts/Reatoken.sol
@@ -7,17 +7,25 @@ import "./AggregatorV3Interface.sol";
 contract Reatoken { 
     address public operator;
     uint256 private immutable _totalSupply;
+    uint256 public reaPriceUSD;
     uint256 public _totalMinted;
     string public _name = "Real Property Token";
     string public _symbol = "RPT";
-    ufixed8x2 public feeRateUSD = 0.01;
+    uint8 public internalDecimal = 2;
+    uint256 feeRateBase;
+    uint8 feeRateBasisPoint;
     mapping(address => uint256) private balanceEth;
     mapping(address => uint256) public balance;
     event TransferRea (address from, address to, uint256 amount);
     event TransferEth (address from, address to, uint256 amount);
 
     constructor () {
-        _totalSupply = 350000;
+        //total token supply is fixed at 3500
+        _totalSupply = 3500;
+        //Each RET Token is priced at $100
+        reaPriceUSD = 10000;
+        feeRateBase = 10000;
+        feeRateBasisPoint = 185;
     }
 
     function totalSupply() public view returns (uint256) {
@@ -57,15 +65,14 @@ contract Reatoken {
     function usdToWeiRate() internal view returns (uint256) {
         ( ,int256 usdToEth, , , ) = priceFeed.latestRoundData();
         (uint8 rateDecimals) = priceFeed.decimals();
-        uint256 usdToWei = uint256(10**(rateDecimals + 18))/uint256(usdToEth);
+        uint256 usdToWei = uint256(10**(rateDecimals + 18 - internalDecimal))/uint256(usdToEth);
         return usdToWei;
     }
 
     function buyInRea (address client, address _operator, uint256 quantityRPT) public payable returns (bool success) {
         require(totalMinted() + quantityRPT <= totalSupply(), "RPT token cap is at 350000");
         uint256 usdToWei = usdToWeiRate();
-        uint256 reaPriceUSD = usdToWei;
-        uint256 paymentInUSD = reaPriceUSD*quantityRPT*(uint256(feeRateUSD) + 1);
+        uint256 paymentInUSD = ((reaPriceUSD*quantityRPT*feeRateBasisPoint) / feeRateBase);
         uint256 weiRequired = paymentInUSD*usdToWei;
         require(msg.value >= weiRequired, "Payment is not sufficient!");
         balanceEth[_operator] += weiRequired;
