31802dea62d9382a8cc42a2461b78f2c78e00c6c
thanh quach <thanhb.quach@gmail.com>
2022-05-17 14:49:55 -0400

    updated function

--- contracts/Reatoken.sol
+++ contracts/Reatoken.sol
@@ -5,41 +5,35 @@ pragma solidity ^0.8.13;
 import "./AggregatorV3Interface.sol";
 
 contract Reatoken { 
-    address payable public operator;
-    uint256 private immutable _cap = 350000;
-    uint256 public totalMinted;
-    string private name = "Real Property Token";
-    string private symbol = "REA";
-    uint8 public reaPriceUSD = 1;
-    uint32 public feeRateUSD = 0.01;
+    address public operator;
+    uint256 private immutable _totalSupply;
+    uint256 public _totalMinted;
+    string public _name = "Real Property Token";
+    string public _symbol = "RPT";
+    ufixed8x2 public feeRateUSD = 0.01;
     mapping(address => uint256) private balanceEth;
     mapping(address => uint256) public balance;
-    event Transfer (address from, address to, uint256 amount);
+    event TransferRea (address from, address to, uint256 amount);
+    event TransferEth (address from, address to, uint256 amount);
 
-
-    struct Property {
-        uint32 priceUSD;
+    constructor () {
+        _totalSupply = 350000;
     }
 
-    constructor (uint256 cap_) {
-        require(cap_ > 0, "REA token cap is at 350000");
-        _cap = cap_;
-    }
-
-    function cap() public view returns (uint256) {
-        return _cap;
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
     }
 
     function name() public view returns (string memory) {
-        return name;
+        return _name;
     }
 
     function symbol() public view returns (string memory) {
-        return symbol;
+        return _symbol;
     }
 
     function totalMinted() public view returns (uint256) {
-        return totalMinted;
+        return _totalMinted;
     }
 
     function checkBalance (address account) public view returns (uint256) {
@@ -48,25 +42,37 @@ contract Reatoken {
 
     error insufficientBalance();
 
-    function sentREA (address receiver, uint256 quantityREA) public {
-        if (quantityREA > balance[operator])
+    function mintRPT (address client, uint256 quantityRPT) internal returns (bool success) {
+        if (quantityRPT > balance[operator])
         revert insufficientBalance();
-        balance[operator] -= quantityREA;
-        balance[receiver] += quantityREA;
-        emit Transfer(operator, receiver, amountREA);
+        balance[operator] -= quantityRPT;
+        balance[client] += quantityRPT;
+        emit TransferRea(operator, client, quantityRPT);
+        return true;
     }
 
-    function usdToWei() {
 
+    AggregatorV3Interface internal priceFeed;
+    
+    function usdToWeiRate() internal view returns (uint256) {
+        ( ,int256 usdToEth, , , ) = priceFeed.latestRoundData();
+        (uint8 rateDecimals) = priceFeed.decimals();
+        uint256 usdToWei = uint256(10**(rateDecimals + 18))/uint256(usdToEth);
+        return usdToWei;
     }
-    function payment (uint256 quantityREA) public {
-        require(totalMinted() + quantityREA <= cap(), "REA token cap is at 350000");
-        uint256 paymentInUSD = reaPriceUSD*quantityREA*(feeRateUSD + 1);
+
+    function buyInRea (address client, address _operator, uint256 quantityRPT) public payable returns (bool success) {
+        require(totalMinted() + quantityRPT <= totalSupply(), "RPT token cap is at 350000");
         uint256 usdToWei = usdToWeiRate();
+        uint256 reaPriceUSD = usdToWei;
+        uint256 paymentInUSD = reaPriceUSD*quantityRPT*(uint256(feeRateUSD) + 1);
         uint256 weiRequired = paymentInUSD*usdToWei;
-        balanceEth[operator] += weiRequired;
-        sentREA(msg.sender, quantityREA);
-        totalSupply += quantity;
+        require(msg.value >= weiRequired, "Payment is not sufficient!");
+        balanceEth[_operator] += weiRequired;
+        emit TransferEth(client, _operator, weiRequired);
+        mintRPT(client, quantityRPT);
+        _totalMinted += quantityRPT;
+        return true;
     }
 
    
