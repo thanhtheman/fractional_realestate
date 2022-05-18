1c70fbe8639cc76d080c32ab2a6e0903eeff1b61
thanh quach <thanhb.quach@gmail.com>
2022-05-18 17:46:45 -0400

    solving the rounding error on dividend distribution

--- contracts/RptToken.sol
+++ contracts/RptToken.sol
@@ -9,14 +9,20 @@ contract RptToken {
     string public _symbol = "RPT";
     uint256 public _totalSupply = 3500;
     uint8 public _decimals = 18;
+    uint256 public totalEarnedDividend;
+    uint256 public dividendBalance;
+    uint256 public alreadyPaidDividend;
+    uint256 public remainingWei;
     event Transfer(address indexed from, address indexed to, uint256 quantityRPT);
 
     mapping (address => uint256) public balanceOfRpt;
+    mapping (address => uint256) public balanceOfDividend;
+    mapping (address => uint256) public balanceOfAlreadyPaidDividend;
 
     constructor () {
-        require(operator == msg.sender);
+        operator == msg.sender;
         balanceOfRpt[operator] = _totalSupply;
-        emit Transfer(address(0), msg.sender, _totalSupply);
+        emit Transfer(address(0), operator, _totalSupply);
     }
 
     function name() public view returns (string memory) {
@@ -35,7 +41,15 @@ contract RptToken {
         return _decimals;
     }
 
-    function transfer(address from, address to, uint256 quantityRPT) public returns (bool success) {
+    modifier updateDividendStatus (address account) {
+       uint256 owedDividend = (totalEarnedDividend - alreadyPaidDividend);
+       balanceOfDividend[msg.sender] += (balanceOfRpt[msg.sender] * owedDividend);
+       balanceOfAlreadyPaidDividend[msg.sender] = totalEarnedDividend;
+        _;
+    }
+
+
+    function transfer(address from, address to, uint256 quantityRPT) public updateDividendStatus(from) updateDividendStatus(to) returns (bool success) {
         require(balanceOfRpt[from] >= quantityRPT, "You don't have sufficient fund!");
         balanceOfRpt[from] -= quantityRPT;
         balanceOfRpt[to] += quantityRPT;
@@ -43,7 +57,30 @@ contract RptToken {
         return true;
     }
 
-    function balanceOf(address _address) public view returns (uint256) {
-        return balanceOfRpt[_address];
+    function balanceOf(address account) public view returns (uint256) {
+        return balanceOfRpt[account];
+    }
+
+    function dividendDeposit(uint256 amount) public payable {
+        require(msg.value == amount);
+        remainingWei = msg.value % _totalSupply;
+        totalEarnedDividend += ((msg.value - remainingWei)/_totalSupply);
+        //There is a rounding difference when we try to divide the dividend amount (wei) to the number of tokens to calculate per-token dividend.
+        //For reconciliation and accurate record, we created a variable to store this "remaining" wei amount 
+        //so that: total received dividend = ditributed dividend + remaining wei.  
+    }
+
+    function checkDividendBalance(address account) public returns (uint256) {
+        uint256 eligibleDividend = balanceOfDividend[account];
+        return eligibleDividend;
+    }
+
+    function withdraw(address account, uint256 amount) public updateDividendStatus(msg.sender) {
+        require(operator != msg.sender);
+        require(amount <= balanceOfDividend[msg.sender], "Insufficient balance to withdraw!");
+        balanceOfDividend[msg.sender] -= amount;
+        msg.sender.transfer(amount);
+        // How can an investor send dividend to his own address from the contract?
+        
     }
 }
