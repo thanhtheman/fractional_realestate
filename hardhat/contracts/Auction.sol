1b31b448e06c7399cd18294fe183c965eef52bf8
thanh quach <thanhb.quach@gmail.com>
2022-05-20 19:28:59 -0400

    updated

---
+++ hardhat/contracts/Auction.sol
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: unlicense
+pragma solidity ^0.8.7;
+import "./RptToken.sol";
+import "./TokenSale.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Auction is RptToken, TokenSale {
+    address public seller;
+    uint256 public startBlock;
+    uint256 public quantityRPTSales;
+    uint256 public floorPriceInWei;
+    uint256 public endBlock;
+    uint256 public bidIncrement;
+    bool public cancelled;
+    bool public sellerWithdrawn;
+    address public highestBidder;
+    uint256 public highestBindingBid;
+    mapping(address => uint256) public bidderFund;
+
+    event NewHighestBid(address bidder, uint256 bid, address highestBidder, uint256 highestBid, uint256 highestBindingBid);
+    event Withdrawn(address withdrawer, address withdrawalAccount, uint256 withdrawalAmount);
+    event AuctionCancelled();
+    event CommissionPaid(address seller, uint256 amount);
+    
+    constructor (address _seller, uint256 _bidIncrement, uint256 _startBlock, uint256 _endBlock) {
+        require(_seller == msg.sender);
+        require(_endBlock > _startBlock);
+        require(block.number > _startBlock);
+        require(block.number < _endBlock);
+
+        seller = _seller;
+        bidIncrement = _bidIncrement;
+        startBlock = _startBlock;
+        endBlock = _endBlock;
+    }
+
+    // This modifier ensure that only the seller can do a specific thing such as cancelling the auction.
+    modifier onlySeller {
+        require(seller == msg.sender);
+        _;
+    }
+
+    // The seller can't submit a bid.
+    modifier onlyNotSeller {
+        require(seller != msg.sender);
+        _;
+    }
+    // You can only bid After the auction starts.
+    modifier onlyAfterStart {
+        require(block.number > startBlock);
+        _;
+    }
+
+     // You can only bid BEFORE the auction is closed.
+    modifier onlyBeforeEnd {
+        require(block.number < endBlock);
+        _;
+    }
+
+    // This is the condition for the contract to accept bids. You can't bid if the auction is cancelled.
+    modifier onlyNotCancelled {
+        require(cancelled = false);
+        _;
+    }
+
+    // This is the condition for all participants, including the seller, to withdraw their funds, only once the auction is ended OR cancelled.
+    modifier onlyEndedOrCancelled {
+        require(block.number > endBlock || cancelled);
+        _;
+    }
+
+    function min(uint256 a, uint256 b) private pure returns(uint256) {
+        if (a < b) return a;
+        return b;
+    }
+    
+    function getHighestBid() public view returns (uint256) {
+        return bidderFund[highestBidder];
+    }
+
+    function getHighesBidingtBid() public view returns (uint256) {
+        return highestBindingBid;
+    }
+
+    //The Seller can the floor (minimum) price and the quanity of RPT tokens he wants to sell. Next,
+    //He will approve/authorize the operator to transfer his sales tokens. 
+
+    function setFloorPriceAndQuantitySales (uint256 _floorPriceInUSD, uint256 _quantityRPTSales) public payable returns (uint256, uint256) {
+        require(seller == msg.sender && balanceOfRpt[seller] >= _quantityRPTSales, "Insufficient tokens to sell!");
+        approve(_quantityRPTSales);
+        quantityRPTSales = _quantityRPTSales;
+        // If the seller input $145 USD, then the equivalent representation of $145 is 14500 on our smart contract. The front-end must do the conversion and feed 14500 to our smart contract.
+        floorPriceInWei = _floorPriceInUSD*usdToWeiRate();
+        return (floorPriceInWei, _quantityRPTSales);
+    }
+
+    function submitBids() public payable onlyAfterStart onlyBeforeEnd onlyNotCancelled onlyNotSeller returns (bool success) {
+        require(msg.value >= floorPriceInWei, "Insufficient funds to bid!");
+        uint256 newBid = bidderFund[msg.sender] + msg.value;
+        require (newBid >= highestBindingBid); //do we need to revert the previous transaction?
+        uint256 highestBid = bidderFund[highestBidder];
+        bidderFund[msg.sender] = newBid;
+        if (newBid <= highestBid) {
+            highestBindingBid = min(newBid + bidIncrement, highestBid);
+        } else {
+            if (msg.sender != highestBidder) {
+                highestBidder = msg.sender;
+                highestBindingBid = min(newBid, highestBid +bidIncrement);
+            }
+            highestBid = newBid;
+        }
+        emit NewHighestBid(msg.sender, newBid, highestBidder, highestBid, highestBindingBid);
+        return true;
+    }
+
+  
+
+    function withdraw() external onlyEndedOrCancelled nonReentrant returns (bool success) {
+        address withdrawalAccount;
+        uint256 withdrawalAmount;
+        
+        if(cancelled) {
+            withdrawalAccount = msg.sender;
+            withdrawalAmount = bidderFund[msg.sender];
+        } else {
+            //Once the auction ends, the tokens are trasnferred from the seller to the winner of the auction
+            transferFrom(seller, highestBidder, quantityRPTSales); 
+            
+            // This is what will happen when the seller withdraws his/her money,
+            // The seller pays the operator a commission fee.
+            // Commission fee is 1.5% of the sale price, which is the highestBindingBid.
+            // Regarding the highest bidder, he/she will be able to withdraw the diffference = highestBid - highestBindingBid
+            if (msg.sender == seller) {
+                uint256 commissionFee = (highestBindingBid + (highestBindingBid*feeRateBasispoint) / feeRateBase); 
+                withdrawalAccount = highestBidder;
+                withdrawalAmount = (highestBindingBid - commissionFee);
+                balanceEth[operator] = commissionFee;
+                (bool commission, ) = operator.call{value: commissionFee}("");  
+                require (commission, "Commission has not been paid");
+                emit CommissionPaid (msg.sender, commissionFee);
+                sellerWithdrawn = true;
+            } else if (msg.sender == highestBidder) {
+                withdrawalAccount = highestBidder;
+                if (sellerWithdrawn) {
+                    withdrawalAmount = bidderFund[highestBidder];
+                } else {
+                    withdrawalAmount =  (bidderFund[highestBidder] - highestBindingBid);
+                }
+                
+            } else {
+                withdrawalAccount = msg.sender;
+                withdrawalAmount = bidderFund[msg.sender];
+            }
+        }
+        
+        //All participants get their money.
+        require(withdrawalAmount != 0);
+        bidderFund[withdrawalAccount] -= withdrawalAmount;
+        (bool withdrawn, ) = msg.sender.call{value: withdrawalAmount}(""); 
+        require(withdrawn, "Withdraw request failed!");
+        emit Withdrawn(msg.sender, withdrawalAccount, withdrawalAmount);
+
+        return true;
+    }
+
+    //The seller has an option to call this function to cancel the auction. However, he/she will needs to pay a cancellation fee to the operator.
+    //The seller will have to pay 1,000,000 GWei for EVERY SINGLE TOKEN that is listed for sales.
+    //Once the auction is , participants can call the withdraw function to get their money back.
+    function cancelAuction() private onlySeller onlyBeforeEnd onlyNotCancelled returns (bool _auctionCancelled) {
+        uint256 cancellationFee = (quantityRPRSales*1000000);
+        (bool cancellation, ) = operator.call{value: cancellationFee}("");  
+        require (cancellation, "cancellation fee has not been paid");
+        cancelled = true;
+        emit AuctionCancelled();
+        return true;
+    }
+}
---
+++ hardhat/contracts/RptToken.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: unlicense
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract RptToken is ReentrancyGuard {
+    address public operator;
+    string public _name = "Real Property Token";
+    string public _symbol = "RPT";
+    uint256 public _totalSupply = 3500;
+    uint8 public _decimals = 18;
+    uint256 public totalEarnedDividend;
+    uint256 public dividendBalance;
+    uint256 public alreadyPaidDividend;
+    uint256 public remainingWei;
+    event Transfer(address indexed from, address indexed to, uint256 quantityRPT);
+    event Approval(address indexed sender, address indexed spender, uint256 quantityRPT);
+
+    mapping (address => uint256) public balanceOfRpt;
+    mapping (address => uint256) public balanceOfDividend;
+    mapping (address => uint256) public balanceOfAlreadyPaidDividend;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    constructor () {
+        operator == msg.sender;
+        balanceOfRpt[operator] = _totalSupply;
+        emit Transfer(address(0), operator, _totalSupply);
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function basicDecimals() public view returns (uint256) {
+        return _decimals;
+    }
+
+    modifier updateDividendStatus (address account) {
+       uint256 owedDividend = (totalEarnedDividend - alreadyPaidDividend);
+       balanceOfDividend[msg.sender] += (balanceOfRpt[msg.sender] * owedDividend);
+       balanceOfAlreadyPaidDividend[msg.sender] = totalEarnedDividend;
+        _;
+    }
+
+
+    function transfer(address from, address to, uint256 quantityRPT) public updateDividendStatus(from) updateDividendStatus(to) returns (bool success) {
+        require(balanceOfRpt[from] >= quantityRPT, "You don't have sufficient fund!");
+        balanceOfRpt[from] -= quantityRPT;
+        balanceOfRpt[to] += quantityRPT;
+        emit Transfer (msg.sender, to, quantityRPT);
+        return true;
+    }
+
+
+    function approve(uint256 quantityRPT) public returns (bool success) {
+        allowance[msg.sender][operator] = quantityRPT;
+        emit Approval(msg.sender, operator, quantityRPT);
+        return true;
+    }
+
+    function transferFrom(address sender, address receiver, uint256 quantityRPT) external returns(bool success) {
+        allowance[sender][operator] -= quantityRPT;
+        balanceOfRpt[sender] -= quantityRPT;
+        balanceOfRpt[receiver] += quantityRPT;
+        emit Transfer(sender, receiver, quantityRPT);
+        return true;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return balanceOfRpt[account];
+    }
+
+    //There is a rounding difference when we try to divide the dividend amount (wei) to the number of tokens to calculate per-token dividend.
+    //For reconciliation and accurate record, we created a variable to store this "remaining" wei amount 
+    //so that: total received dividend = ditributed dividend + remaining wei.
+    //There is still one problem - if we set this function as public, anyone can call it and access to the way we calculate dividend? 
+    function dividendDeposit(uint256 amount) public payable {
+        require(msg.value == amount);
+        remainingWei = msg.value % _totalSupply;
+        totalEarnedDividend += ((msg.value - remainingWei)/_totalSupply); 
+    }
+
+    
+
+    function checkDividendBalance(address account) public view returns (uint256) {
+        uint256 eligibleDividend = balanceOfDividend[account];
+        return eligibleDividend;
+    }
+
+    function withdraw(uint256 amount) public updateDividendStatus(msg.sender) nonReentrant {
+        require(operator != msg.sender);
+        require(amount <= balanceOfDividend[msg.sender], "Insufficient balance to withdraw!");
+        balanceOfDividend[msg.sender] -= amount;
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Dividend transfer failed!");
+        // How can an investor send dividend to his own address from the contract?
+        
+    }
+}
+
+// We will need to write 2 functions transferFrom and approve to enable contracts to transfer tokens so that we can transfer token from seller
+//to buyer in the auction
